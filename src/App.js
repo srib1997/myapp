76f79135997b49d1c71f10ceeb7c02b0db396e48
srib <peterf1a08@yahoo.com.hk>
2019-02-17 11:23:53 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, { Analytics, Storage } from 'aws-amplify';
+import { withAuthenticator, S3Album } from 'aws-amplify-react';
 import awsmobile from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react';
+
 Amplify.configure(awsmobile);
+Storage.configure({ level: 'private' });
 
 class App extends Component {
+
+  uploadFile = (evt) => {
+    const file = evt.target.files[0];
+    const name = file.name;
+
+    Storage.put(name, file).then(() => {
+      this.setState({ file: name });
+    })
+  }
+
+  componentDidMount() {
+    Analytics.record('Amplify_CLI');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p> Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />
+        <S3Album level="public" path='' />
       </div>
     );
   }
